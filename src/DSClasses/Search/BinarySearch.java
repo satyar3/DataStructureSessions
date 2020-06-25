e467c846b9f8f874426021225c8c64a2b0b9fc1d
Satyaranjan Muduli <satyaranjan.muduli@amadeus.com>
2020-06-25 21:24:33 +0530

    Practice

--- src/DSClasses/Search/BinarySearch.java
+++ src/DSClasses/Search/BinarySearch.java
@@ -7,7 +7,7 @@ public class BinarySearch
 		int arr[] = {3,4,5,6,7,8};
 		int low = 0;
 		int high = arr.length-1;
-		System.out.println(binarySearch(arr, 9, low, high));
+		System.out.println(binarySearch(arr, 8, low, high));
 	}
 	
 	public static int binarySearch(int arr[], int searchVal, int low, int high)
